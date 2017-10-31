8e192f45ccf9ce1d3ee0f7434900c16076dca5ec
Kael Shipman <kael.shipman@cfxtrading.com>
2017-10-31 18:36:19 -0500

    Set up orders client to be fleshed out later

--- src/Client.php
+++ src/Client.php
@@ -7,6 +7,7 @@ class Client extends \CFX\Persistence\Rest\AbstractDataContext implements Client
 
     protected function instantiateDatasource($name) {
         if ($name == 'assets') return new AssetsClient($this);
+        if ($name == 'orders') return new OrdersClient($this);
 
         throw new UnknownDatasourceException("Programmer: Don't know how to handle datasources of type `$name`. If you'd like to handle this, you should either add this datasource to the `instantiateClient` method in this class or create a derivative class and add it there.");
     }
